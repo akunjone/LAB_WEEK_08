77276fb704dedbe7ad6064715b4794fef661e8d1
akunjone <103233070+akunjone@users.noreply.github.com>
2025-10-27 10:53:59 +0700

    Initial commit + “Commit No. 1 – add first & second worker”

---
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.lab_week_08
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.lab_week_08.worker.FirstWorker
+import com.example.lab_week_08.worker.SecondWorker
+
+class MainActivity : AppCompatActivity() {
+    //create an instance of a work manager
+    //work manager manages all your requests and workers
+    //it also sets up the sequenct for all your processes
+    private val workManager = WorkManager.getInstance(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //create a constraint of which your workers are bound to
+        //here the workers cannot execute the given process if
+        //there's no internet connection
+        val networkConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val Id = "001"
+
+        //2 types of work request:
+        //OneTimeWorkRequest and PeriodWorkRequest
+        //OneTimeWorkRequest exec the request just once
+        //PeriodicWorkRequest exec periodically
+
+        //one time work request that includes all constraints and inputs needed for the worker
+        //created request for FirstWorker class
+        val firstRequest = OneTimeWorkRequest
+            .Builder(FirstWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(FirstWorker
+                .INPUT_DATA_ID, Id)
+            ).build()
+
+        //ini buat second worker
+        val secondRequest = OneTimeWorkRequest
+            .Builder(SecondWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(SecondWorker
+                .INPUT_DATA_ID, Id)
+            ).build()
+
+        //sets up the process sequence from the work manager instance
+        //here starts with firstworker, then secondworker
+        workManager.beginWith(firstRequest)
+            .then(secondRequest)
+            .enqueue()
+
+        //skrg getting outputnya
+        //kita receive output, display result as toast
+        //ada LiveData dan observe:
+        //LiveData = data holder class in Android Jetpack supaya lebih reactive
+        //reaktif itu datang dari observe keyword
+        //observing data changes and immediately update app UI
+
+        //kita observe returned LiveData and getting state result of worker (SUCCEEDED, FAILED, CANCELLED)
+        //isFinished untuk cek apakah state SUCCEEDED / FAILED
+        workManager.getWorkInfoByIdLiveData(firstRequest.id)
+            .observe(this){ info ->
+                if (info != null) {
+                    if(info.state.isFinished){
+                        showResult("First process is done")
+                    }
+                }
+            }
+
+        workManager.getWorkInfoByIdLiveData(secondRequest.id)
+            .observe(this){ info ->
+                if (info != null) {
+                    if(info.state.isFinished){
+                        showResult("Second process is done")
+                    }
+                }
+            }
+    }
+
+    //FUNCTIONS
+    //build data into the correct format before passing it to the worker as input
+    private fun getIdInputData(IdKey: String, IdValue: String) =
+        Data.Builder()
+            .putString(IdKey, IdValue)
+            .build()
+
+    //show result as toast
+    private fun showResult(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/com/example/lab_week_08/worker/SecondWorker.kt
@@ -0,0 +1,28 @@
+package com.example.lab_week_08.worker
+
+import android.content.Context
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class SecondWorker(
+    context: Context, workerParams: WorkerParameters
+) : Worker(context, workerParams){
+    override fun doWork(): Result{
+        //get the parameter input
+        val id = inputData.getString(INPUT_DATA_ID)
+        //sleep the process for 3s
+        Thread.sleep(3000L)
+        //build output based on process result
+        val outputData = Data.Builder()
+            .putString(OUTPUT_DATA_ID, id)
+            .build()
+
+        //return output
+        return Result.success(outputData)
+    }
+    companion object{
+        const val INPUT_DATA_ID = "inId"
+        const val OUTPUT_DATA_ID = "outId"
+    }
+}
